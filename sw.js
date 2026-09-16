76026d515d9941d40cf18f9b8b556b561efc3e5b
nbuysch13 <n.buysch@gmail.com>
2026-09-16 14:11:38 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+/* Bei jedem neuen Upload die Zahl erhöhen. */
+const CACHE = "feldbuch-v1";
+const FILES = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-maskable-512.png","./apple-touch-icon.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(caches.match(e.request).then(h => h || fetch(e.request).catch(() => caches.match("./index.html"))));
+});
